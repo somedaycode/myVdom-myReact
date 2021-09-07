7a930003b9fd525ed4aeea5fbb3b68930230a32f
somedaycode <somedaycode@gmail.com>
2021-09-07 17:09:46 +0900

    fix: 이벤트가 중복으로 등록되는 문제 해결
    * 앱이 렌더링 될 때마다 중복되는 이벤트를 클로저 변수를 통해 제거 후 등록
    
    * 문제발견: 현재 하나의 이벤트만 등록이 된다.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { updateDom } from './myVdom.js';
 const $root = document.querySelector('#app');
 
 export const eventHandler = (() => {
+  let callBackState = null;
   const addEvent = (eventType, cb) => {
+    $root.removeEventListener(eventType, callBackState);
     $root.addEventListener(eventType, cb);
+    callBackState = cb;
   };
 
   return {
