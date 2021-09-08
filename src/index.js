83d9f964208f9bcb7b90435504566beec79fdb88
somedaycode <somedaycode@gmail.com>
2021-09-09 00:05:52 +0900

    feat: key를 통해 여러 이벤트를 제어

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import { updateDom } from './myVdom.js';
 const $root = document.querySelector('#app');
 
 export const eventHandler = (() => {
-  let callBackState = null;
-  const addEvent = (eventType, cb) => {
-    $root.removeEventListener(eventType, callBackState);
-    $root.addEventListener(eventType, cb);
-    callBackState = cb;
+  let callBackState = [];
+  let key = 0;
+
+  const addEvent = (eventType, selector, cb) => {
+    key = 0;
+    setTimeout(() => {
+      const $target = $root.querySelector(selector);
+
+      $target.removeEventListener(eventType, callBackState[key]);
+      $target.addEventListener(eventType, cb);
+      callBackState[key] = cb;
+      key++;
+    }, 0);
   };
 
   return {
@@ -17,7 +25,7 @@ export const eventHandler = (() => {
 })();
 
 function init() {
-  const renderd = App();
+  const renderd = App({ $target: $root });
   updateDom($root, renderd);
 }
 
